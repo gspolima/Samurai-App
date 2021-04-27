fbdaf35af24a6f5bdd4cf9339b43b7c0f80baed7
Gustavo Sampaio <sampaioglima@gmail.com>
2021-04-27 20:40:01 -0300

    Add DateJoined property to BattleSamurai join domain class.

---
+++ SamuraiApp.Domain/BattleSamurai.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SamuraiApp.Domain
+{
+    public class BattleSamurai
+    {
+        public int BattleId { get; set; }
+        public int SamuraiId { get; set; }
+        public DateTime DateJoined { get; set; }
+    }
+}
