4307cb16c1e1774cb87ff78ba56994b9def40b6d
Gustavo Sampaio <sampaioglima@gmail.com>
2021-05-07 01:07:29 -0300

    Add Start and End dates to Battle.

---
+++ SamuraiApp.Data/Migrations/20210507040609_AddStartAndEndDate2Battle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SamuraiApp.Data.Migrations
+{
+    public partial class AddStartAndEndDate2Battle : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "EndDate",
+                table: "Battles",
+                type: "datetimeoffset",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "StartDate",
+                table: "Battles",
+                type: "datetimeoffset",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EndDate",
+                table: "Battles");
+
+            migrationBuilder.DropColumn(
+                name: "StartDate",
+                table: "Battles");
+        }
+    }
+}
