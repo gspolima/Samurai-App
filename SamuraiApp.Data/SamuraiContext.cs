bc91a96f59dfd63943cf3937fb4240d9b0073d6a
Gustavo Sampaio <sampaioglima@gmail.com>
2021-05-30 23:31:17 -0300

    Mapping keyless object to a database view.

--- SamuraiApp.Data/SamuraiContext.cs
+++ SamuraiApp.Data/SamuraiContext.cs
@@ -11,6 +11,7 @@ namespace SamuraiApp.Data
         public DbSet<Quote> Quotes { get; set; }
         public DbSet<Battle> Battles { get; set; }
         public DbSet<Horse> Horses { get; set; }
+        public DbSet<SamuraiBattleStats> SamuraiBattleStats { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
@@ -38,6 +39,10 @@ namespace SamuraiApp.Data
                  bs => bs.HasOne<Samurai>().WithMany())
                 .Property(bs => bs.DateJoined)
                 .HasDefaultValueSql("getdate()");
+
+            modelBuilder.Entity<SamuraiBattleStats>()
+                .HasNoKey()
+                .ToView("[dbo].[SamuraiBattleStats]");
         }
     }
 }
---
+++ SamuraiApp.Domain/SamuraiBattleStats.cs
@@ -0,0 +1,9 @@
+namespace SamuraiApp.Domain
+{
+    public class SamuraiBattleStats
+    {
+        public string Name { get; set; }
+        public int? NumberOfBattles { get; set; }
+        public string EarliestBattle { get; set; }
+    }
+}
