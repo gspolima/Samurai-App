d974d31dbc8298ddefc152598154b00616e408cd
Gustavo Sampaio <sampaioglima@gmail.com>
2021-06-04 22:03:28 -0300

    Add property to flag if SamuraId has value or not.

--- SamuraiApp.Domain/Quote.cs
+++ SamuraiApp.Domain/Quote.cs
@@ -5,6 +5,22 @@
         public int Id { get; set; }
         public string Text { get; set; }
         public Samurai Samurai { get; set; }
-        public int SamuraiId { get; set; }
+        public int SamuraiId
+        { 
+            get
+            {
+                return _samuraiId;
+            }
+            set
+            {
+                _samuraiId = value;
+                _hasValue = true;
+            }
+        }
+
+        public bool HasValue => _hasValue;
+
+        private int _samuraiId;
+        private bool _hasValue;
     }
 }
