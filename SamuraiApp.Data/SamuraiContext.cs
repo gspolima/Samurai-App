82c721fc56ce2e5274b0c74a14c79d4e441c9b8e
Gustavo Sampaio <sampaioglima@gmail.com>
2021-06-10 04:41:38 -0300

    Reintroduce provider configuration.

--- SamuraiApp.Data/SamuraiContext.cs
+++ SamuraiApp.Data/SamuraiContext.cs
@@ -9,6 +9,12 @@ namespace SamuraiApp.Data
         {
         }
 
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                "Data Source= (localdb)\\MSSQLLocalDB;Initial Catalog=SamuraiTestData");
+        }
+
         public DbSet<Samurai> Samurais { get; set; }
         public DbSet<Quote> Quotes { get; set; }
         public DbSet<Battle> Battles { get; set; }
