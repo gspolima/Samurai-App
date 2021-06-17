66130506c7d6fae0305cbe753c238ec35102a693
Gustavo Sampaio <sampaioglima@gmail.com>
2021-06-17 20:22:16 -0300

    Remove Swagger from the DI Container.

--- SamuraiApp.Api/Startup.cs
+++ SamuraiApp.Api/Startup.cs
@@ -1,17 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
 using SamuraiApp.Data;
 
 namespace SamuraiApp.Api
@@ -29,15 +21,12 @@ namespace SamuraiApp.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            services.AddDbContext<SamuraiContext>(options  =>
+            services.AddDbContext<SamuraiContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("SamuraiConnection"))
                 .EnableSensitiveDataLogging()
                 .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking));
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "SamuraiApp.Api", Version = "v1" });
-            });
+            services.AddScoped<BusinessLogicData>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -46,8 +35,6 @@ namespace SamuraiApp.Api
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "SamuraiApp.Api v1"));
             }
 
             app.UseHttpsRedirection();
